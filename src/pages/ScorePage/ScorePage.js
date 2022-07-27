d02a44bdce6b3b4d43b6b8374dcd88ae5f3f917d
TijanaJelic <tijanajelic.jelic@gmail.com>
2022-07-27 21:22:49 +0200

    Added result and design

---
+++ src/pages/ScorePage/ScorePage.js
@@ -0,0 +1,24 @@
+import React, { useContext } from 'react';
+import { Link } from 'react-router-dom';
+import ParametersContext from '../../components/Context';
+import './score.css';
+
+const ScorePage = ({ finalScore }) => {
+  const parameters = useContext(ParametersContext);
+  console.log(parameters);
+
+  return (
+    <div className="score-container">
+      Your score is{' '}
+      <span className="result">
+        <span className="num-box">{finalScore}</span> of{' '}
+        <span className="num-box">{parameters[0][1]}</span>
+      </span>
+      <Link to={'/'} className="back-home-bttn">
+        Back home
+      </Link>
+    </div>
+  );
+};
+
+export default ScorePage;
